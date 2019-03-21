93f66d69ca3e67e3ea339a3cd614abc6ed85e44f
Subkhon Amrulloev <subkhon.amrulloev@blackrock.com>
2019-03-21 07:29:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function takeANumber(line, name){
+  var line_length=line.length;
+  var number=line_length+1;
+  line.push(name);
+  return `Welcome, ${name}. You are number ${number} in line.`;
+}
+
+function nowServing(line){
+  if(line.length===0){
+    return "There is nobody waiting to be served!";
+  }else{
+    var cur_person=line[0];
+    line.shift();
+    return cur_person;
+  }
+}
